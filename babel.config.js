625feed906cc4f4ff9ea9980ef40e50a2391bf92
talanova <talanova@speechpro.com>
2021-05-29 16:40:36 +0300

    setup proj

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+// babel.config.js
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: { node: "current" },
+        useBuiltIns: "entry", // alternative mode: "entry"/ "usage"
+        corejs: 3, // default would be 2
+      },
+    ],
+    "@babel/preset-typescript",
+  ],
+  plugins: ["@babel/plugin-proposal-class-properties"],
+};
